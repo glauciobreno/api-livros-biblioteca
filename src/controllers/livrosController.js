a404d4caa4493b17adc33a3dbbc46cc95b6b445b
glaucio breno <glauciobreno@gmail.com>
2022-11-04 11:45:34 -0300

    cadastrando livro na api

--- src/controllers/livrosController.js
+++ src/controllers/livrosController.js
@@ -7,6 +7,19 @@ class livroController {
             res.status(200).json(livros)
     })
     }
-}
+
+    static cadastrarLivro = (req, res) => {
+        let livro = new livros(req.body);
+
+        livro.save((err) => {
+            
+            if(err) {
+                res.status(500).send({message: `${err.message} falha ao cadastrar livro`})
+            } else {
+                res.status(201).send(livro.toJSON())
+            }
+        })
+    }
+    }
 
 export default livroController
--- src/routes/livrosRoutes.js
+++ src/routes/livrosRoutes.js
@@ -5,5 +5,6 @@ const router = express.Router();
 
 router
  .get("/livros", livroController.listarLivros)
+ .post("/livros", livroController.cadastrarLivro)
 
  export default router;
